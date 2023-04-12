bca9cb478677521eb94203ef12b2795fade6739a
Jason Alberto <alberto.jason@gmail.com>
2023-04-11 23:38:38 -0400

    Add lock images with styling and toggle function

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var lockButton = document.querySelector('.main-display');
+
+lockButton.addEventListener('click', function(event) {
+    if (event.target.className === 'lock-box') {
+        lockToggle(event);
+    }
+});
+
 function getRandomHex() {
     return (Math.floor(Math.random() * 16777216).toString(16).padStart(6, 0));
+}
+
+function lockToggle(event) {
+    if (event.target.src === 'file:///Users/jason/turing/1mod/projects/coloRandom/assets/unlocked.png') {
+        event.target.src = 'file:///Users/jason/turing/1mod/projects/coloRandom/assets/locked.png'
+    } else {
+        event.target.src = 'file:///Users/jason/turing/1mod/projects/coloRandom/assets/unlocked.png'
+    }
 }
