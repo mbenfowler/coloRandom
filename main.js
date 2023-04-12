1f82a824ca27d201e86003654e449d7930aecc57
Matt Fowler <mbfowler@gmail.com>
2023-04-12 09:36:39 -0400

    Add randomized hex functionality

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-// When the page initially loads, is refreshed, or the button is clicked: eventListener
-// -- 5 random and new colors should appear
-// -- the color’s hex codes should appear below each color box
-// -- the current palette should be tracked somewhere in your Data Model and updated when a new palette is generated
 var hexData = [];
 var currentHexes;
 
-var newPalatteButtonSection = document.querySelectorAll('section:last-of-type');
-var newPalatteButton = document.querySelectorAll('.button-boxL, button, .button-boxL');
-var mainColorBoxes = document.querySelectorAll('.box-title');
+var newPaletteButtonSection = document.querySelector('.button-area');
+var newPaletteButton = document.querySelector('button');
+
+var mainColorBoxes = document.querySelectorAll('.color-container');
 
 window.addEventListener('load', function () {
     console.log("getNewHexes()");
 });
 
-newPalatteButton.addEventListener('click', getNewHexes)
-
-// newPalatteButtonSection.addEventListener('click', function(event) {
-//     console.log(event);
-// });
-
+newPaletteButtonSection.addEventListener('click', function(event) {
+    if(event.target.classList.contains('button-box-l') || event.target.id === 'new-palette' || event.target.classList.contains('button-box-r')) {
+        getNewHexes();
+    }
+});
 
 function getNewHexes() {
     currentHexes = [];
-    for(i = 0; i <= 5; i++) {
-        currentHexes.push(getRandomhex());
-        console.log(mainColorBoxes[i]);
-    }
+    mainColorBoxes.forEach((colorBox) => {
+        var newColor = getRandomHex().toUpperCase();
+        colorBox.firstElementChild.style.backgroundColor = `#${newColor}`;
+        colorBox.lastElementChild.innerText = `#${newColor}`;
+    });
 }
 
 function getRandomHex() {
